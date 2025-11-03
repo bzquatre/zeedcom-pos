d777c43cca8c826da18dfc4a8c24135a42e76d91
bzquatre <bzquatre@gmail.com>
2025-11-03 14:34:27 +0100

    INIT

--- src-pos/com/openbravo/pos/forms/AppLocal.java
+++ src-pos/com/openbravo/pos/forms/AppLocal.java
@@ -30,12 +30,12 @@ public class AppLocal {
     /**
      *
      */
-    public static final String APP_NAME = "uniCenta oPOS";
+    public static final String APP_NAME = "zeedcom oPOS";
 
     /**
      *
      */
-    public static final String APP_ID = "unicentaopos";
+    public static final String APP_ID = "zeedcomopos";
 
     /**
      *
